7246bc1a527df92c4bf51cfd3d3acdc0ce81df69
KwanhDg <101886812+KwanhDg@users.noreply.github.com>
2025-04-20 22:10:31 +0700

    auto switch banner

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -8,6 +8,35 @@ import { RouterLink } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
   email: string = 'info@letgonow.com';
+  private bannerTimer: any;
+
+  ngOnInit() {
+    this.startBannerSwitching();
+  }
+
+  ngOnDestroy() {
+    if (this.bannerTimer) {
+      clearInterval(this.bannerTimer);
+    }
+  }
+
+  startBannerSwitching() {
+    this.bannerTimer = setInterval(() => this.bannerSwitcher(), 5000);
+  }
+
+  bannerSwitcher() {
+    const inputs = document.querySelectorAll('.sec-1-input') as NodeListOf<HTMLInputElement>;
+    let currentIndex = Array.from(inputs).findIndex(input => input.checked);
+    let nextIndex = (currentIndex + 1) % inputs.length;
+    inputs[nextIndex].checked = true;
+  }
+
+  onControlClick() {
+    if (this.bannerTimer) {
+      clearInterval(this.bannerTimer);
+    }
+    this.startBannerSwitching();
+  }
 }
